a5b08b0aa5a97c21d71eee59d7d62de5c994b3d4
Shivani Sukale <sukaleshivani@gmail.com>
2022-03-16 17:13:06 +0530

    Multiplication Successful

--- src/com/harman/calculator/Calculator.java
+++ src/com/harman/calculator/Calculator.java
@@ -4,12 +4,16 @@ public class Calculator {
 
     public static void main(String[] args) {
         Substraction subc= new Substraction();
+        Multiplication mulc=new Multiplication();
         int a=5,b=6,c;
         c= a+b;
         System.out.println(c);
 
         int result=subc.sub(a,b);
         System.out.println(result);
+        int result2= mulc.mul(a,b);
+        System.out.println(result2);
+
 
     }
 }
---
+++ src/com/harman/calculator/Multiplication.java
@@ -0,0 +1,7 @@
+package com.harman.calculator;
+
+public class Multiplication {
+    public int mul(int x,int y){
+        return x*y;
+    }
+}
